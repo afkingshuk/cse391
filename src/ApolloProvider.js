684c0d9cc72e423d5db54f14792699fe47d4f44e
afkingshuk <afkingshuk@gmail.com>
2021-12-19 20:37:38 +0600

    file structure and backend

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://cse391-milestone-3.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
